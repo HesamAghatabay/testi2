0f40fb4728a6f820afd6bb2f3dd4a900bd74ac5f
Hesam Aghatabay <program.h.p.2023@gmail.com>
2025-03-24 12:25:49 +0330

    update

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -2,15 +2,40 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use Nette\Utils\Random;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::resource('register', UserController::class);
 Route::middleware('auth:api')->resource('profile', ProfileController::class);
+Route::post('/sendVerify', function (Request $request) {
+    $code = rand(1000, 9999);
+    if ($user = User::where('mobile', $request->username)->first()) {
+        $user->password = $code;
+        $user->save();
+        return response()->json(['status' => true, 'message' => 'user already exist']);
+    } else {
+        $user = User::create([
+            'name' => '',
+            'email' => '',
+            'mobile' => $request->username,
+            'password' => $code,
+        ]);
+    }
+    $user->sendVerifyCode($code);
+    if ($user->sendVerifyCode($code, $request->username)) {
+        return response()->json(['status' => true], 200);
+    } else {
+        return response()->json(['status' => false, 'message' => 'sms faild'], 400);
+    }
+});
+
+
 
 
 Route::post('logout', function (Request $request) {
